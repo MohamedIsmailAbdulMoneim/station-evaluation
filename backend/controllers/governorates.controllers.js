ad0f5dc4eca5f0de6242039da46e80d6d86fbb9e
Mohamed Khodeir <mkhodeir@ngvc.local>
2025-03-09 10:07:21 +0200

    feat

--- backend/controllers/governorates.controllers.js
+++ backend/controllers/governorates.controllers.js
@@ -3,14 +3,16 @@ const { pool } = require("../connection");
 const { logMessage } = require("../utils/system.utils");
 
 const getGovernoratesController = async (req, res) => {
+  const { areaId } = req.query;
+
   let client;
 
   logMessage("Hit get governorates endpoint", "Success", "Info");
 
   try {
     client = await pool.connect();
-    const query = `select governorate_id as id, governorate_name as name from governorates`;
-    const result = await client.query(query);
+    const query = `select governorate_id as id, governorate_name as name from governorates where area_id = $1`;
+    const result = await client.query(query, [areaId]);
 
     logMessage("governorates was fetched successfuly", "Success", "Info");
     return res.json({ data: result.rows, status: "success" });
--- backend/controllers/stations.controller.js
+++ backend/controllers/stations.controller.js
@@ -3,13 +3,15 @@ const { pool } = require("../connection");
 const { logMessage } = require("../utils/system.utils");
 
 const getStationsController = async (req, res) => {
+  const { govId } = req.query;
+
   let client;
 
   logMessage("Hit get stations endpoint", "Success", "Info");
 
   try {
     client = await pool.connect();
-    const query = `select station_id as id, station_name as name from stations`;
+    const query = `select station_id as id, station_name as name from stations where governorate_id = ${govId}`;
     const result = await client.query(query);
 
     logMessage("stations was fetched successfuly", "Success", "Info");
