ba4dfaee39ce9b5f6a50dd84b1642c562a5d5f09
Mohamed Khodeir <mkhodeir@ngvc.local>
2025-03-16 12:08:43 +0200

    fix: rollback

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 require("dotenv").config();
-const https = require("https"); // Add the https module
-const fs = require("fs"); // Add the fs module
-const path = require("path"); // Add the path module
+const app = express();
 const cors = require("cors");
 
 const questions = require("./routes/questions");
@@ -14,10 +12,8 @@ const answers = require("./routes/answers");
 
 const { logMessage } = require("./utils/system.utils");
 
-const port = process.env.PORT || 3000; // Default to port 3000 if not specified
+const port = process.env.PORT;
 const logLevel = process.env.LOG_LEVEL || "tiny";
-
-const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -28,17 +24,6 @@ app.use("/api/v1/areas", areas);
 app.use("/api/v1/governorates", governorates);
 app.use("/api/v1/answers", answers);
 
-// Load SSL certificate and private key
-const options = {
-  key: fs.readFileSync(path.join(__dirname, "selfsigned.key")),
-  cert: fs.readFileSync(path.join(__dirname, "selfsigned.crt")),
-};
-
-// Create an HTTPS server
-https.createServer(options, app).listen(port, () => {
-  logMessage(
-    `Forms is running on https://localhost:${port}`,
-    "Success",
-    "INFO"
-  );
+app.listen(port, () => {
+  logMessage(`Forms is running on http://localhost:${port}`, "Success", "INFO");
 });
